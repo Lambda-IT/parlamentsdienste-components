7a4b8ddf2086972ce84d507b38433a5666e1340e
Thomas Schneiter <t.schneiter@gmx.net>
2020-04-15 16:16:19 +0200

    removed reset mixin

--- src/globals/globals.ts
+++
@@ -1,8 +0,0 @@
-import { getMode } from '@stencil/core';
-import { Mode } from '../interface';
-
-let defaultMode: Mode;
-
-export const getIonMode = (ref?: any): Mode => {
-    return (ref && getMode(ref)) || defaultMode;
-};
