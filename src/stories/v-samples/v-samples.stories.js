777a20b7fa10c9724ce1903445428630e99cd6e6
Thomas Schneiter <59556890+thomasschneiter@users.noreply.github.com>
2020-07-01 08:33:36 +0200

    fixed link

--- src/stories/v-samples/v-samples.stories.js
+++ src/stories/v-samples/v-samples.stories.js
@@ -1,11 +1,11 @@
 export default {
-    title: 'vuejs|samples'
+    title: 'vuejs|samples',
 };
 
 export const sampleApp = () => `
 <div class="text-center m-5">
     <h1>vuejs samples</h1>
     <p>A vuejs sample app which shows how to use the webcomponents in different situations<p/>
-    <a href="/vue.html" target="_blank" >vuejs sample app</a>
+    <a href="/vue" target="_blank" >vuejs sample app</a>
     </div>
   `;
