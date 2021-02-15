785715e1f78a5a586953b59e058f3a638762e7fe
Thomas Schneiter <thomas.schneiter@lambda-it.ch>
2021-02-15 15:54:01 +0100

    updated script path to use getAssetPath as default if no meta is available

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,16 +1,17 @@
+import { getAssetPath } from '@stencil/core';
+
 export const namespace = 'parlamentsdienstecore'; // this should be imported into your stencil.config.ts file and used as the namespace for your project
 
-let currentScriptUrl: string;
+let customScriptUrl: string = null;
 
-export function getCurrentScriptUrl(): string {
-    if (!currentScriptUrl) {
-        currentScriptUrl =
-            document.querySelector('meta[name="parlamentsdienste-base-path"]')?.getAttribute('content') ||
-            document.baseURI;
+export function getCustomScriptUrl(): string {
+    if (!customScriptUrl) {
+        customScriptUrl = document.querySelector('meta[name="parlamentsdienste-base-path"]')?.getAttribute('content');
     }
-    return currentScriptUrl;
+    return customScriptUrl;
 }
 
 export function getURL(path: string): string {
-    return new URL(path, getCurrentScriptUrl()).href;
+    const scriptUrl = getCustomScriptUrl();
+    return !!scriptUrl ? new URL(path, scriptUrl).href : getAssetPath(path);
 }
