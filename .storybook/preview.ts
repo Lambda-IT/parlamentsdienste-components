d7c7f3d85c00ca72f6605111b5f6c28accc494bc
Thomas Schneiter <thomas.schneiter@lambda-it.ch>
2023-10-17 13:20:20 +0200

    reverted popper import

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
 import { Preview } from '@storybook/html';
 import DocumentationTemplate from '../src/stories/DocumentationTemplate.mdx';
 
+import 'https://unpkg.com/@popperjs/core@2';
+import 'https://unpkg.com/tippy.js@6';
 import '../dist/parlamentsdienstecore/parlamentsdienstecore.css';
 import '../dist/parlamentsdienstecore/parlamentsdienstecore.esm.js';
 import '../dist/parlamentsdienstecore/typography.css';
