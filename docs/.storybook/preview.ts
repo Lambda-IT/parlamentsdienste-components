877af6e02fb6037f64580cf2236ac24d6c4ddf14
Tom Wenger <tom.wenger@lambda-it.ch>
2025-06-10 15:54:18 +0200

    added pd-alert to storybook

--- docs/.storybook/preview.ts
+++ docs/.storybook/preview.ts
@@ -1,17 +1,18 @@
 /** @type { import('@storybook/html-vite').Preview } */
 import { defineCustomElements } from '@parlamentsdienste-components/core/loader';
-
+import '@parlamentsdienste-components/core/styles/parlamentsdienstecore.css';
+import '@parlamentsdienste-components/core/styles/typography.css';
 defineCustomElements();
 
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
 };
 
 export default preview;
