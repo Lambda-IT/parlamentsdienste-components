0a20abc715a150a11a875e63621e4560e78897d0
Tom Wenger <tom.wenger@lambda-it.ch>
2025-06-16 10:18:12 +0200

    wip

---
+++ packages/core/components/pd-alert.js
@@ -0,0 +1,172 @@
+import { p as proxyCustomElement, H, c as createEvent, h } from './index.js';
+
+const collapse = (element) => {
+    // remove this event listener so it only gets triggered once
+    if (element.trDone)
+        element.removeEventListener('transitionend', element.trDone);
+    // get current height of element
+    var sectionHeight = element.scrollHeight;
+    // remove transition from element
+    var elementTransition = element.style.transition;
+    element.style.transition = '';
+    element.style.overflow = 'hidden';
+    // in next frame, set height from auto to actual height of element and add transition again
+    requestAnimationFrame(function () {
+        element.style.height = sectionHeight + 'px';
+        element.style.transition = elementTransition;
+        // now set height to 0 and start transition
+        requestAnimationFrame(function () {
+            element.style.height = '0';
+        });
+    });
+};
+const expand = (element) => {
+    // get expanded height of element
+    var sectionHeight = element.scrollHeight;
+    // set height to expanded
+    element.style.height = sectionHeight + 'px';
+    // wait for transition to end
+    element.addEventListener('transitionend', (element.trDone = function transitionDone() {
+        // set height back to auto when transition is done
+        element.style.height = null;
+        element.style.overflow = 'visible';
+    }));
+};
+
+const pdAlertCss = "input,textarea,a,button{font:1rem/1.5rem Avenir;letter-spacing:0.19px;margin:0}button,[type=button],[type=reset],[type=submit]{appearance:none}:host{display:block}*,::before,::after{box-sizing:border-box;outline:none}.pd-alert{border-radius:0.25rem;border-width:0.25rem;border-style:solid;font-weight:700}.pd-alert .pd-alert-top-section{display:flex;flex-wrap:nowrap;flex-direction:row;padding:0.625rem 1rem}.pd-alert .pd-alert-top-section .pd-alert-message-action{width:100%;display:flex;flex-wrap:wrap;flex-direction:row;justify-content:space-between;column-gap:1rem}.pd-alert .pd-alert-top-section .pd-alert-icon{margin:-0.5rem 0.5rem -0.5rem -0.5rem;width:2.5rem;min-width:2.5rem;align-self:center}.pd-alert .pd-alert-top-section .pd-alert-close-button{cursor:pointer;appearance:none;background-color:transparent;padding:0;margin:0;border:none;margin-left:0.625rem}.pd-alert .pd-alert-top-section .pd-alert-close-button:focus-visible{background-color:#ffec99;color:#033840;fill:#033840}.pd-alert .pd-alert-top-section .pd-alert-close-button:focus-visible .pd-alert-action-cancel{background-color:#ffec99;color:#033840;fill:#033840}.pd-alert.pd-alert-primary{background-color:#0b7285;border-color:#0b7285;color:#ffffff;fill:#ffffff}.pd-alert.pd-alert-primary .pd-alert-action-text{color:#ffffff}.pd-alert.pd-alert-primary .pd-alert-action-cancel:hover{fill:#15aabf}.pd-alert.pd-alert-primary .pd-alert-action-cancel:active{fill:#66d9e8}.pd-alert.pd-alert-danger{background-color:#fa5252;border-color:#fa5252;color:#0b0b0b;fill:#0b0b0b}.pd-alert.pd-alert-danger .pd-alert-action-text{color:#0b0b0b}.pd-alert.pd-alert-danger .pd-alert-action-cancel:hover{fill:#c92a2a}.pd-alert.pd-alert-danger .pd-alert-action-cancel:active{fill:#ffa8a8}.pd-alert.pd-alert-warning{background-color:#fab005;border-color:#fab005;color:#0b0b0b;fill:#0b0b0b}.pd-alert.pd-alert-warning .pd-alert-action-text{color:#0b0b0b}.pd-alert.pd-alert-warning .pd-alert-action-cancel:hover{fill:#df7900}.pd-alert.pd-alert-warning .pd-alert-action-cancel:active{fill:#ffe066}.pd-alert.pd-alert-info{background-color:#4dadf7;border-color:#4dadf7;color:#0b0b0b;fill:#0b0b0b}.pd-alert.pd-alert-info .pd-alert-action-text{color:#0b0b0b}.pd-alert.pd-alert-info .pd-alert-action-cancel:hover{fill:#1864ab}.pd-alert.pd-alert-info .pd-alert-action-cancel:active{fill:#74c0fc}.pd-alert.pd-alert-success{background-color:#82c91e;border-color:#82c91e;color:#0b0b0b;fill:#0b0b0b}.pd-alert.pd-alert-success .pd-alert-action-text{color:#0b0b0b}.pd-alert.pd-alert-success .pd-alert-action-cancel:hover{fill:#5c940d}.pd-alert.pd-alert-success .pd-alert-action-cancel:active{fill:#c0eb75}.pd-alert.pd-alert-dark{background-color:#343a40;border-color:#343a40;color:#ffffff;fill:#ffffff}.pd-alert.pd-alert-dark .pd-alert-action-text{color:#ffffff}.pd-alert.pd-alert-dark .pd-alert-action-cancel:hover{fill:#bababa}.pd-alert.pd-alert-dark .pd-alert-action-cancel:active{fill:#dee2e6}.pd-alert.pd-alert-light{background-color:#f8f9fa;border-color:#f8f9fa;color:#0b0b0b;fill:#0b0b0b}.pd-alert.pd-alert-light .pd-alert-action-text{color:#0b0b0b}.pd-alert.pd-alert-light .pd-alert-action-cancel:hover{fill:#dee2e6}.pd-alert.pd-alert-light .pd-alert-action-cancel:active{fill:#dee2e6}.pd-alert-action-text{padding:0;cursor:pointer;font-weight:700 !important;background-color:transparent;border:none;text-decoration:underline;text-align:left}.pd-alert-action-text:focus-visible{background-color:#ffec99;color:#033840;fill:#033840}.pd-alert-action-cancel{cursor:pointer;margin:-0.5rem -0.5rem -0.5rem 0.5rem;transform:translateY(-0.25rem)}.pd-alert-action-cancel:hover{fill:#e3e0e0}.pd-alert-expandable-content-wrapper{background-color:var(--pd-alert-expandable-background-color, #ffffff);color:var(--pd-alert-expandable-color, #000);font-weight:400;transition:height 0.3s ease-in-out;height:auto}.pd-alert-expandable-content-wrapper .pd-alert-expandable-content{padding:1rem}";
+
+const Alert = /*@__PURE__*/ proxyCustomElement(class Alert extends H {
+    constructor() {
+        super();
+        this.__registerHost();
+        this.__attachShadow();
+        this.pdClosed = createEvent(this, "pd-closed");
+        this.pdAction = createEvent(this, "pd-action");
+        this.pdCollapsed = createEvent(this, "pd-collapsed");
+    }
+    contentWrapperElement;
+    /** Color schema used for the alert */
+    color = 'primary';
+    /** Display an option to close the alert */
+    closable = false;
+    /** Text to show on action */
+    actionText;
+    /** Text to show on expanded action */
+    actionTextExpanded;
+    /** A link displayed to the right side of the alert */
+    actionHref;
+    /** Target for action href (eg. _blank) */
+    actionTarget = '_blank';
+    /** Hide alert icon */
+    hideIcon = false;
+    /** Enable expandable content */
+    expandable = false;
+    /** Expands / collapses the panel content */
+    expanded = false;
+    valueChanged(expanded) {
+        if (expanded) {
+            expand(this.contentWrapperElement);
+        }
+        else {
+            collapse(this.contentWrapperElement);
+        }
+    }
+    /** Emitted when close button was pressed. */
+    pdClosed;
+    /** Emitted when action button was pressed. */
+    pdAction;
+    /** Emitted when inner content is expanded/collapsed. */
+    pdCollapsed;
+    componentDidLoad() {
+        // start collapsed
+        if (!this.expanded) {
+            this.contentWrapperElement.style.height = '0';
+            this.contentWrapperElement.style.overflow = 'hidden';
+        }
+    }
+    handleAction() {
+        if (this.expandable)
+            this.expanded = !this.expanded;
+        if (this.expandable)
+            this.pdCollapsed.emit(!this.expanded);
+        this.pdAction.emit();
+    }
+    render() {
+        return (h("div", { key: 'f8ee873f9b7da42076ad3d7f1504eb7549b8b2ae', class: {
+                'pd-alert': true,
+                [`pd-alert-${this.color}`]: true,
+            } }, h("div", { key: 'f4d35d25e22e832a9712387119f9334bae3799e0', class: "pd-alert-top-section" }, this.renderIcon(), h("div", { key: '83970b0727a2bee0a70966b411b0bd9a2fb3451a', class: "pd-alert-message-action" }, h("slot", { key: '20a2cfcda78f59cb58585c51f43a535d65e1940c' }), this.renderAction()), this.renderClose()), h("div", { key: 'dce416599fc64310affbd6e635f18b2a20b59e9a', class: "pd-alert-expandable-content-wrapper", ref: el => (this.contentWrapperElement = el) }, this.renderExpandable())));
+    }
+    renderExpandable() {
+        if (!this.expandable)
+            return;
+        return (h("div", { class: "pd-alert-expandable-content" }, h("slot", { name: "expandable" })));
+    }
+    renderAction() {
+        const { actionHref, actionText, actionTarget, actionTextExpanded } = this;
+        if (!this.actionText)
+            return;
+        const TagType = actionHref ? 'a' : 'button';
+        const typeAttrs = TagType === 'button' ? { type: 'button' } : { href: actionHref, target: actionTarget };
+        return (h("div", null, h(TagType, { class: "pd-alert-action-text", ...typeAttrs, onClick: () => this.handleAction(), "data-test": "pd-alert-action" }, !this.expanded ? actionText : actionTextExpanded)));
+    }
+    renderClose() {
+        if (!this.closable)
+            return;
+        return (h("div", null, h("button", { class: "pd-alert-close-button", onClick: this.pdClosed.emit, "data-test": "pd-alert-close" }, h("pd-icon", { class: "pd-alert-action-cancel", name: "close", size: 2 }))));
+    }
+    renderIcon() {
+        if (this.hideIcon)
+            return;
+        switch (this.color) {
+            case 'danger':
+                return h("pd-icon", { class: "pd-alert-icon", size: 2.5, name: "alert_danger" });
+            case 'info':
+                return h("pd-icon", { class: "pd-alert-icon", size: 2.5, name: "alert_info" });
+            case 'success':
+                return h("pd-icon", { class: "pd-alert-icon", size: 2.5, name: "confirm" });
+            case 'warning':
+                return h("pd-icon", { class: "pd-alert-icon", size: 2.5, name: "alert_warning" });
+            default:
+                return;
+        }
+    }
+    static get watchers() { return {
+        "expanded": ["valueChanged"]
+    }; }
+    static get style() { return pdAlertCss; }
+}, [1, "pd-alert", {
+        "color": [1],
+        "closable": [4],
+        "actionText": [1, "action-text"],
+        "actionTextExpanded": [1, "action-text-expanded"],
+        "actionHref": [1, "action-href"],
+        "actionTarget": [1, "action-target"],
+        "hideIcon": [4, "hide-icon"],
+        "expandable": [4],
+        "expanded": [1028]
+    }, undefined, {
+        "expanded": ["valueChanged"]
+    }]);
+function defineCustomElement$1() {
+    if (typeof customElements === "undefined") {
+        return;
+    }
+    const components = ["pd-alert"];
+    components.forEach(tagName => { switch (tagName) {
+        case "pd-alert":
+            if (!customElements.get(tagName)) {
+                customElements.define(tagName, Alert);
+            }
+            break;
+    } });
+}
+defineCustomElement$1();
+
+const PdAlert = Alert;
+const defineCustomElement = defineCustomElement$1;
+
+export { PdAlert, defineCustomElement };
+//# sourceMappingURL=pd-alert.js.map
+
+//# sourceMappingURL=pd-alert.js.map
---
+++ packages/core/components/pd-input.js
@@ -0,0 +1,244 @@
+import { p as proxyCustomElement, H, c as createEvent, h, a as Host } from './index.js';
+
+const pdInputCss = "input,textarea,a,button{font:1rem/1.5rem Avenir;letter-spacing:0.19px;margin:0}button,[type=button],[type=reset],[type=submit]{appearance:none}:host{display:block}*,::before,::after{box-sizing:border-box;appearance:none;outline:none}.pd-input-label{width:100%;margin-bottom:0;max-width:100%}.pd-input-label .pd-input-label-text{font-size:0.875rem;padding-bottom:0.25rem;overflow:hidden;text-overflow:ellipsis}.pd-input{display:block;padding:0.625rem 0.875rem;border-radius:0.25rem;border:0.125rem solid #0b7285;width:100%;margin-top:var(--pd-input-vertical-adjust, inherit)}.pd-input:hover:enabled{border-color:#15aabf}.pd-input:disabled{border-color:#cfcfcf;background-color:transparent;font-style:italic;color:#0b0b0b}.pd-input:focus-visible:enabled{background:#fff3bf}.pd-input.pd-input-readonly{background-color:#dee2e6;border-color:#dee2e6}.pd-input.pd-input-readonly:hover:enabled,.pd-input.pd-input-readonly:disabled,.pd-input.pd-input-readonly:focus-visible:enabled{border-color:#dee2e6;cursor:default;background-color:#dee2e6}.pd-input.pd-input-error{border-color:#b33529;background:#ffa8a8}.pd-input.pd-input-error:hover:enabled{border-color:#c92a2a}.pd-input-label-viewonly{font-weight:700}.pd-input-viewonly{display:block;margin:0;padding:0.125rem 0 0.75rem 0}";
+
+const Input = /*@__PURE__*/ proxyCustomElement(class Input extends H {
+    constructor() {
+        super();
+        this.__registerHost();
+        this.__attachShadow();
+        this.pdInput = createEvent(this, "pd-input");
+        this.pdChange = createEvent(this, "pd-change");
+        this.pdBlur = createEvent(this, "pd-blur");
+        this.pdFocus = createEvent(this, "pd-focus");
+    }
+    nativeInput;
+    /**
+     * If the value of the type attribute is `"file"`, then this attribute will indicate the types of files that the server accepts, otherwise it will be ignored. The value must be a comma-separated list of unique content type specifiers.
+     */
+    accept;
+    /**
+     * Indicates whether and how the text value should be automatically capitalized as it is entered/edited by the user.
+     */
+    autocapitalize = 'off';
+    /**
+     * Indicates whether the value of the control can be automatically completed by the browser.
+     */
+    autocomplete = 'off';
+    /**
+     * Whether auto correction should be enabled when the user is entering/editing the text value.
+     */
+    autocorrect = 'off';
+    /**
+     * This Boolean attribute lets you specify that a form control should have input focus when the page loads.
+     */
+    autofocus = false;
+    /**
+     * If `true`, a clear icon will appear in the input when there is a value. Clicking it clears the input.
+     */
+    clearInput = false;
+    /**
+     * If `true`, the value will be cleared after focus upon edit. Defaults to `true` when `type` is `"password"`, `false` for all other types.
+     */
+    clearOnEdit;
+    /**
+     * If `true`, the user cannot interact with the input.
+     */
+    disabled = false;
+    /**
+     * If `true`, the input is replaced with a simple text
+     */
+    viewOnly = false;
+    /**
+     * A hint to the browser for which keyboard to display.
+     * Possible values: `"none"`, `"text"`, `"tel"`, `"url"`,
+     * `"email"`, `"numeric"`, `"decimal"`, and `"search"`.
+     */
+    inputmode;
+    /**
+     * The maximum value, which must not be less than its minimum (min attribute) value.
+     */
+    max;
+    /**
+     * If the value of the type attribute is `text`, `email`, `search`, `password`, `tel`, or `url`, this attribute specifies the maximum number of characters that the user can enter.
+     */
+    maxlength;
+    /**
+     * The minimum value, which must not be greater than its maximum (max attribute) value.
+     */
+    min;
+    /**
+     * If the value of the type attribute is `text`, `email`, `search`, `password`, `tel`, or `url`, this attribute specifies the minimum number of characters that the user can enter.
+     */
+    minlength;
+    /**
+     * If `true`, the user can enter more than one value. This attribute applies when the type attribute is set to `"email"` or `"file"`, otherwise it is ignored.
+     */
+    multiple;
+    /**
+     * The name of the control, which is submitted with the form data.
+     */
+    name;
+    /**
+     * A regular expression that the value is checked against. The pattern must match the entire value, not just some subset. Use the title attribute to describe the pattern to help the user. This attribute applies when the value of the type attribute is `"text"`, `"search"`, `"tel"`, `"url"`, `"email"`, or `"password"`, otherwise it is ignored.
+     */
+    pattern;
+    /**
+     * Instructional text that shows before the input has a value.
+     */
+    placeholder;
+    /**
+     * If `true`, the user cannot modify the value.
+     */
+    readonly = false;
+    /**
+     * If `true`, the user must fill in a value before submitting a form.
+     */
+    required = false;
+    /**
+     * Works with the min and max attributes to limit the increments at which a value can be set.
+     * Possible values are: `"any"` or a positive floating point number.
+     */
+    step;
+    /**
+     * The initial size of the control. This value is in pixels unless the value of the type attribute is `"text"` or `"password"`, in which case it is an integer number of characters. This attribute applies only when the `type` attribute is set to `"text"`, `"search"`, `"tel"`, `"url"`, `"email"`, or `"password"`, otherwise it is ignored.
+     */
+    size = 1;
+    /**
+     * The type of control to display. The default type is text.
+     */
+    type = 'text';
+    /**
+     * The value of the input.
+     */
+    value = '';
+    /**
+     * Label text
+     */
+    label;
+    /**
+     * Shows error state
+     */
+    error = false;
+    /**
+     * Default vertical adjustment for inline forms
+     */
+    verticalAdjust = false;
+    /**
+     * Emitted when a keyboard input occurred.
+     */
+    pdInput;
+    /**
+     * Emitted when the value has changed.
+     */
+    pdChange;
+    /**
+     * Emitted when the input loses focus.
+     */
+    pdBlur;
+    /**
+     * Emitted when the input has focus.
+     */
+    pdFocus;
+    /**
+     * Update the native input element when the value changes
+     */
+    valueChanged() {
+        this.pdChange.emit({ value: this.value == null ? this.value : this.value.toString() });
+    }
+    /**
+     * Sets focus on the specified `pd-input`. Use this method instead of the global
+     * `input.focus()`.
+     */
+    async setFocus() {
+        if (this.nativeInput) {
+            this.nativeInput.focus();
+        }
+    }
+    onInput = (ev) => {
+        const input = ev.target;
+        if (input) {
+            this.value = input.value || '';
+        }
+        this.pdInput.emit(ev);
+    };
+    onBlur = () => {
+        if (!this.disabled || !this.readonly)
+            this.pdBlur.emit();
+    };
+    onFocus = () => {
+        this.pdFocus.emit();
+    };
+    onKeydown = () => { };
+    getValue() {
+        return typeof this.value === 'number' ? this.value.toString() : (this.value || '').toString();
+    }
+    render() {
+        const value = this.getValue();
+        return (h(Host, { key: 'cfbd3b9ac0dbdec741f8416f63dc55435c477765' }, h("label", { key: '275d474ad86b18f06998f175b52ee97fc60572c4', class: "pd-input-label" }, this.label ? (h("div", { class: {
+                'pd-input-label-text': true,
+                'pd-input-label-viewonly': this.viewOnly,
+            }, "data-test": "pd-input-label" }, this.label)) : (''), !this.viewOnly ? (h("input", { class: {
+                'pd-input': true,
+                'pd-input-readonly': this.readonly,
+                'pd-input-error': this.error,
+            }, ref: input => (this.nativeInput = input), disabled: this.disabled, accept: this.accept, autoCapitalize: this.autocapitalize, autoComplete: this.autocomplete, autoCorrect: this.autocorrect, autoFocus: this.autofocus, inputMode: this.inputmode, min: this.min, max: this.max, minLength: this.minlength, maxLength: this.maxlength, multiple: this.multiple, name: this.name, pattern: this.pattern, placeholder: this.placeholder || '', readonly: this.readonly, required: this.required, step: this.step, size: this.size, type: this.type, value: value, onInput: this.onInput, onBlur: this.onBlur, onFocus: this.onFocus, onKeyDown: this.onKeydown, style: this.verticalAdjust ? { '--pd-input-vertical-adjust': '1.5625rem' } : {}, "data-test": "pd-input", tabIndex: this.readonly ? -1 : undefined })) : (h("p", { class: "pd-input-viewonly" }, this.value)))));
+    }
+    static get watchers() { return {
+        "value": ["valueChanged"]
+    }; }
+    static get style() { return pdInputCss; }
+}, [1, "pd-input", {
+        "accept": [1],
+        "autocapitalize": [1],
+        "autocomplete": [1],
+        "autocorrect": [1],
+        "autofocus": [4],
+        "clearInput": [4, "clear-input"],
+        "clearOnEdit": [4, "clear-on-edit"],
+        "disabled": [4],
+        "viewOnly": [4, "view-only"],
+        "inputmode": [1],
+        "max": [1],
+        "maxlength": [2],
+        "min": [1],
+        "minlength": [2],
+        "multiple": [4],
+        "name": [1],
+        "pattern": [1],
+        "placeholder": [1],
+        "readonly": [4],
+        "required": [4],
+        "step": [1],
+        "size": [2],
+        "type": [1],
+        "value": [1032],
+        "label": [1],
+        "error": [4],
+        "verticalAdjust": [4, "vertical-adjust"],
+        "setFocus": [64]
+    }, undefined, {
+        "value": ["valueChanged"]
+    }]);
+function defineCustomElement$1() {
+    if (typeof customElements === "undefined") {
+        return;
+    }
+    const components = ["pd-input"];
+    components.forEach(tagName => { switch (tagName) {
+        case "pd-input":
+            if (!customElements.get(tagName)) {
+                customElements.define(tagName, Input);
+            }
+            break;
+    } });
+}
+defineCustomElement$1();
+
+const PdInput = Input;
+const defineCustomElement = defineCustomElement$1;
+
+export { PdInput, defineCustomElement };
+//# sourceMappingURL=pd-input.js.map
+
+//# sourceMappingURL=pd-input.js.map
