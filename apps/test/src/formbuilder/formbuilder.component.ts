dc07ac1944da217c3d7270b337b0a925a9f4b994
Tom Wenger <tom.wenger@lambda-it.ch>
2025-07-01 11:23:06 +0200

    reactive forms wip

--- apps/test/src/formbuilder/formbuilder.component.ts
+++ apps/test/src/formbuilder/formbuilder.component.ts
@@ -1,39 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-    PdAlert,
-    PdButton,
-    PdCheckbox,
-    PdCombobox,
-    PdDatepicker,
-    PdDropdownItem,
-    PdIcon,
-    PdInput,
-} from '@parlamentsdienste-components/angular';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PdButton, PdCheckbox, PdDatepicker, PdInput } from '@parlamentsdienste-components/angular';
 
 @Component({
     selector: 'app-formbuilder',
     templateUrl: './formbuilder.component.html',
     styleUrls: ['./formbuilder.component.css'],
-    imports: [
-        CommonModule,
-        PdAlert,
-        PdInput,
-        FormsModule,
-        PdIcon,
-        PdButton,
-        PdCombobox,
-        PdDropdownItem,
-        PdCheckbox,
-        ReactiveFormsModule,
-        PdDatepicker,
-    ],
+    imports: [CommonModule, PdInput, FormsModule, PdButton, PdCheckbox, ReactiveFormsModule, PdDatepicker],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormbuilderComponent {
     testForm = new FormGroup({
-        name: new FormControl('1234'),
+        name: new FormControl('1234', Validators.minLength(3)),
+        name2: new FormControl('asdf'),
         checkbox: new FormControl(true),
         date: new FormControl('2025-07-23'),
     });
@@ -42,5 +22,11 @@ export class FormbuilderComponent {
         this.testForm.valueChanges.subscribe(value => {
             console.log('Form value changed:', value);
         });
+    }
+
+    buttonClicked() {
+        console.log('Button clicked');
+        console.log(this.testForm.get('name')?.disable());
+        // this.testForm.get('name2')!.invalid = true;
     }
 }
