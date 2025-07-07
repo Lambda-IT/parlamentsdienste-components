41f82b80005802756a37b67e50861bef4a884e9c
Tom Wenger <tom.wenger@lambda-it.ch>
2025-07-07 13:53:42 +0200

    add react test app

---
+++ apps/react-test/src/app/app.tsx
@@ -0,0 +1,12 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+
+export function App() {
+    return (
+        <div>
+            <h1>Welcome to React Test!</h1>
+        </div>
+    );
+}
+
+export default App;
