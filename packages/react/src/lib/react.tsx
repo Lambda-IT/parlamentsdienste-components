f261d2c9c186330c547d36e3350ce92cec653269
Tom Wenger <tom.wenger@lambda-it.ch>
2025-07-07 13:23:01 +0200

    add react library

---
+++ packages/react/src/lib/react.tsx
@@ -0,0 +1,11 @@
+import styles from './react.module.css';
+
+export function React() {
+    return (
+        <div className={styles['container']}>
+            <h1>Welcome to React!</h1>
+        </div>
+    );
+}
+
+export default React;
