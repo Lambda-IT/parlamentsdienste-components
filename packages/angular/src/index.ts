2d6e52c2d207a5ea408c29f84c81e4fb7386ecf4
Tom Wenger <tom.wenger@lambda-it.ch>
2025-07-15 10:36:04 +0200

    increase bundle budgets

--- packages/angular/src/index.ts
+++ packages/angular/src/index.ts
@@ -1,4 +1,2 @@
 export * from './lib/angular/components';
-// export * from './lib/angular/selfmade';
-// export * from './lib/angular/text-value-accessor';
-export * from './lib/angular/value-accessor';
+// export * from './lib/angular/value-accessor';
