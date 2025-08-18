90b999b8add78dcbb8165c4e26b945b0dbd1105f
Tom Wenger <tom.wenger@lambda-it.ch>
2025-08-18 10:26:17 +0200

    fixed e2e tests

--- packages/core/src/components/pd-modal/test/pd-modal.e2e.ts
+++ packages/core/src/components/pd-modal/test/pd-modal.e2e.ts
@@ -3,9 +3,20 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('pd-modal', () => {
     it('renders', async () => {
         const page = await newE2EPage();
-        await page.setContent('<pd-modal></pd-modal>');
+        await page.evaluate(() => {
+            const modal = document.createElement('pd-modal');
+            modal.config = { title: 'Test Modal', zIndex: '1000', backdropVisible: true };
+            modal.innerHTML = `
+                <p>Modal Content</p>
+                <div slot="footer">
+                    <pd-button outline>Cancel</pd-button>
+                    <pd-button>Save</pd-button>
+                </div>`;
+            document.body.appendChild(modal);
+        });
+        await page.waitForChanges();
 
         const element = await page.find('pd-modal');
-        expect(element).toHaveClass('hydrated');
+        expect(element).not.toBeNull();
     });
 });
